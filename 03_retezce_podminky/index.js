be32f6cf32d7bb07dd4e6607c930efcf2d46f325
Ivuska <fischerova.ivka@gmail.com>
2023-02-07 11:14:28 +0100

    Solution to recommended home excercises.

--- 03_retezce_podminky/index.js
+++ 03_retezce_podminky/index.js
@@ -1,4 +1,5 @@
 /* Prace s retezci */
+/*
 
 // 1. Vlastnosti a metody
 
@@ -46,7 +47,7 @@ document.body.innerHTML = address;
 
 /* Podminky */
 
-
+/*
 // 1. registrace na ockovani
 
 const jmeno = prompt("Zadejte jmeno:");
@@ -62,7 +63,9 @@ if (vek >= 65) {
 if (heslo.length < 8) {
     document.body.innerHTML += "Koukej si vymyslet poradne dlouhy heslo!<br>";
 }
+*/
 
+/*
 
 // 2. Cena vstupenky
 
@@ -82,3 +85,40 @@ if (age <= 6) {
     plnaCena = 0.50 * plnaCena;
     document.body.innerHTML = `"Divadlo te bude stat ${plnaCena} euro."`;
 };
+
+*/
+
+/* Doporucene ulozky na doma */
+
+// 1. FIT email
+/*
+const name = prompt("Krestni jmeno bez diakritiky:").trim().slice(0,3);
+const surname = prompt("Prijmeni bez diakritiky:").trim().slice(0,4);
+
+const emailAddress = surname + name + '@' + 'fit.cvut.cz';
+
+document.body.innerHTML = `
+   "Tvoje emailova adresa je ${emailAddress}.";
+`
+*/
+// 2. Mnozstevni slevy
+
+const numberOfTshirts = Number(prompt("Kolik objednavate kusu?"))
+
+let price = 0;
+
+if (numberOfTshirts < 50 ) {
+   price = 300;
+} else if (numberOfTshirts >= 50 && numberOfTshirts <= 200) {
+    price = 250;
+} else if (numberOfTshirts > 200 && numberOfTshirts <= 500) {
+    price = 200;
+} else if (numberOfTshirts > 500 && numberOfTshirts <= 1000) {
+    price = 150;
+} else {
+   price = 120;
+}
+
+document.body.innerHTML = `
+    Za jedno tricko zaplatite ${price} Kc.
+`
