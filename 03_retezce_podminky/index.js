e14693ed18e0412f0a8328193cf8e36286e42f02
Ivuska <fischerova.ivka@gmail.com>
2023-02-07 10:50:40 +0100

    Excercises from lesson.

--- 03_retezce_podminky/index.js
+++ 03_retezce_podminky/index.js
@@ -11,7 +11,7 @@ console.log(title.slice(title.length - 5, title.length));
 
 // 2. Emaily
 
-const email = prompt('Zadejte emailovou adresu:');
+ const email = prompt('Zadejte emailovou adresu:');
 
 const atIndex = email.indexOf('@');
 const userName = email.slice(0, atIndex);
@@ -41,4 +41,44 @@ const address =
     </div>
     `   
 
-document.body.innerHTML = address;
+document.body.innerHTML = address; 
+
+
+/* Podminky */
+
+
+// 1. registrace na ockovani
+
+const jmeno = prompt("Zadejte jmeno:");
+const vek = Number(prompt("Kolik vam je?"));
+const heslo = prompt("Zadejte heslo:");
+
+if (vek >= 65) {
+    document.body.innerHTML += "Vas vek je v poradku, pokracujte.<br>";
+} else {
+    document.body.innerHTML +="Sorry, na ockovani si jeste musis pockat.<br>";
+}
+
+if (heslo.length < 8) {
+    document.body.innerHTML += "Koukej si vymyslet poradne dlouhy heslo!<br>";
+}
+
+
+// 2. Cena vstupenky
+
+const age = Number(prompt("Kolik je ti let?"));
+
+let plnaCena = 12;
+
+if (age <= 6) {
+    plnaCena = 0 * plnaCena;
+    document.body.innerHTML = `"Divadlo te bude stat ${plnaCena} euro."`;
+} else if (age <= 26) {
+    plnaCena = 0.65 * plnaCena;
+    document.body.innerHTML = `"Divadlo te bude stat ${plnaCena} euro."`;
+} else if (age >= 27 && age <= 64) {
+    document.body.innerHTML = `"Divadlo te bude stat ${plnaCena} euro."`;
+} else  {
+    plnaCena = 0.50 * plnaCena;
+    document.body.innerHTML = `"Divadlo te bude stat ${plnaCena} euro."`;
+};
