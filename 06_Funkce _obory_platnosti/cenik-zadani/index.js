77bab4ee9ca559621dc7cd90719093ebbc2475ed
Ivuska <fischerova.ivka@gmail.com>
2023-02-09 14:27:06 +0100

    Working solution for third at home excercise

--- 06_Funkce _obory_platnosti/cenik-zadani/index.js
+++ 06_Funkce _obory_platnosti/cenik-zadani/index.js
@@ -1,20 +1,15 @@
 const planNumber = Number(prompt('Vyber si plan:')) 
 
 const selectPlan = (planNumber) => {
-    let elem;
+    let elem = document.querySelector("#plan1"); 
+    elem.classList.remove("plan--selected");
 
-    if (planNumber === 1) {
-        elem = document.querySelector("#plan1");
-        elem.classList.toggle("plan--selected");
-    } else if (planNumber === 2) {
-        elem = document.querySelector("#plan2");
-        elem.classList.toggle("plan--selected");
-    } else if (planNumber === 3) {
-        elem = document.querySelector("#plan3");
-        elem.classList.toggle("plan--selected");
+    if (planNumber >= 1 && planNumber <=3) {
+        elem = document.querySelector(`#plan${planNumber}`);
+        elem.classList.toggle("plan--selected");   
     } else {
-    return "Takovyhle plan nemame."  
-    }
+        return "Takovyhle plan nemame."  
+    };
 };
 
 console.log(selectPlan(planNumber));
