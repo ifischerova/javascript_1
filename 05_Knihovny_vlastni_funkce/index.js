56dc578b3f82c3af533a003a9ac2b80718b75ddf
Ivuska <fischerova.ivka@gmail.com>
2023-02-09 09:37:12 +0100

    Solution of excercises from lesson.

--- 05_Knihovny_vlastni_funkce/index.js
+++ 05_Knihovny_vlastni_funkce/index.js
@@ -1,6 +1,7 @@
 /* Cviceni Knihovny */
 
 // 1. Zakladni validace
+/*
 const email = prompt('Zadejte emailovou adresu:')
 
 const elem = document.querySelector("#msg");
@@ -12,3 +13,61 @@ if (validator.isEmail(email) === true) {
     elem.innerHTML = "Neplatný email";
     elem.classList.add('msg--invalid');
 };
+*/
+
+/* Cviceni funkce */
+
+// 1. Obsah elipsy
+
+const pi = Math.PI;
+
+const ellipseArea = (width,  height, pi) => {
+    return width / 2 * height / 2 * pi
+}
+
+console.log(ellipseArea(2,2,pi));
+
+// 2. Maximum ze dvou cisel
+/*
+const prvniCislo = Number(prompt("Prvni cislo:"));
+const druheCislo = Number(prompt("Druhe cislo:"));
+
+
+const max2 = (prvniCislo, druheCislo) => {
+    if (prvniCislo === druheCislo) {
+        return "Cisla se rovnaji";
+    } else if (prvniCislo > druheCislo) {
+        return prvniCislo;
+    } else {
+        return druheCislo;
+    }
+};
+
+console.log(max2(prvniCislo, druheCislo));
+*/
+
+// 3. Kontrola DIČ
+
+const inputStr = prompt('Zadejte DIC:');
+
+const prefix = inputStr.slice(0,2);
+
+const digits = inputStr.slice(2);
+
+const isDIC = (inputStr, prefix, digits) => {
+    if (inputStr.length < 11 || inputStr >= 12){
+        return false;
+    }
+
+    if (prefix !== 'CZ') {
+        return false;
+    }
+
+    if (!validator.isInt(digits)) {
+        return false;
+    }
+    
+    return true;
+};
+
+console.log(isDIC(inputStr, prefix, digits));
