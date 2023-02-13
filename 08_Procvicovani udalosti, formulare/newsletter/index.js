17a79139cad69bc0af436e881424602344ccc3d4
Ivuska <fischerova.ivka@gmail.com>
2023-02-13 10:35:50 +0100

    Solution for more complex form excercise.

--- 08_Procvicovani udalosti, formulare/newsletter/index.js
+++ 08_Procvicovani udalosti, formulare/newsletter/index.js
@@ -6,3 +6,12 @@ formElm.addEventListener('submit', (event) => {
     const emailAddressValue = emailAddress.value;
     formElm.innerHTML = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${emailAddressValue}.`
 })
+
+const orderForm = document.querySelector('#orderForm');
+
+orderForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    orderForm.innerHTML = `<h1>Hotovo</h1>
+    <p>Objednavka odeslana ke zpracovani.</p>
+    `
+})
